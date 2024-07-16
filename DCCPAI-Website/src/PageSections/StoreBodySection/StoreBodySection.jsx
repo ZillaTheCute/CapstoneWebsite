ed952d52e43b828f5666111811d3cf0597083e08
Andrei Lee <andrei22.lee@gmail.com>
2024-07-16 15:15:54 +0800

    More Shop Page Progress - Page list hover state has to be fixed + product card hover state too

--- DCCPAI-Website/src/PageSections/StoreBodySection/StoreBodySection.jsx
+++ DCCPAI-Website/src/PageSections/StoreBodySection/StoreBodySection.jsx
@@ -5,13 +5,26 @@ function ShopBodySection() {
     return(
     <>
     <div className={styles.shopBodyContainer}>
-
+        <div className={styles.shopBodyLeft}>
         <h1>Categories</h1>
+        <h2>All Products</h2>
+        <h2>Cacao</h2>
         <ul>
-            <li>Cacao</li>
-            <li>Non-Cacao</li>
+            <li>Product Type 1</li>
+            <li>Product Type 2</li>
+            <li>Product Type 2</li>
+            <li>Product Type 3</li>
         </ul>
 
+        <h2>Non Cacao</h2>
+        <ul>
+            <li>Product Type 1</li>
+            <li>Product Type 2</li>
+        </ul>
+
+        </div>
+        
+        <div className={styles.shopBodyRight}>
         <div className={styles.shopBodyGrid}>
             <ProductCard />
             <ProductCard />
@@ -23,6 +36,14 @@ function ShopBodySection() {
             <ProductCard />
             <ProductCard />
         </div>
+        <div className={styles.pageList}>
+                <div><h2>1</h2></div>
+                <div><h2>2</h2></div>
+                <div><h2>3</h2></div>
+                <div><h2>4</h2></div>
+            </div>
+        </div>
+
     </div>
     </>
     );
