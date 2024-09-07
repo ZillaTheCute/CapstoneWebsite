2dedf8007658f8285b7ef1ecf3a53f4d4aae483a
Andrei Lee <andrei22.lee@gmail.com>
2024-09-07 17:16:06 +0800

    Created ProductCard2, overhauled ourCacaoSection

--- DCCPAI-Website/src/PageSections/OurCacaoSection/OurCacaoSection.jsx
+++ DCCPAI-Website/src/PageSections/OurCacaoSection/OurCacaoSection.jsx
@@ -1,18 +1,30 @@
 import styles from './OurCacaoSection.module.css';
 import TextButton from '../../TextButton/TextButton';
+import ProductCard from '../../ProductCard/ProductCard.jsx'
+import ProductCard2 from '../../ProductCard2/ProductCard2.jsx'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; // Import Swiper styles
 
 function OurCacaoSection() {
   return (
     <div className={styles.ourCacaoSection}>
-      <div className={styles.ourCacaoMainText}>
-        Our Cacao
+
+      <div className={styles.ourCacaoSectionTitle}>
+        <div className={styles.ourCacaoMainText}>
+          Our Cacao
+        </div>
+
+        <div className={styles.ourCacaoSubText}>
+          Distinctly Davao. (make this a carousel or not)
+        </div>
       </div>
-      <div className={styles.ourCacaoSubText}>
-        Distinctly Davao
+      
+      <div className={styles.ourCacaoProducts}>
+        <ProductCard2 /> 
+        <ProductCard2 /> 
+        <ProductCard2 /> 
       </div>
-      <div className={styles.buttonContainer}>
-      <TextButton buttonText="Shop!" buttonLink="Shop" className={styles.buttonContainer}></TextButton>
-      </div>
+
     </div>
   );
 }
---
+++ DCCPAI-Website/src/ProductCard2/ProductCard2.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './ProductCard2.module.css';
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import TextButton from '../TextButton/TextButton';
+
+function ProductCard2(props) {
+
+    return (
+        <div className={styles.productCardContainer}>
+
+            <div className={styles.topRow}>
+                <div className={styles.productReview}>
+                    Reviews
+                </div>
+                <div className={styles.productPrice}>
+                    $Price
+                </div>
+            </div>
+
+            <div className={styles.middlePicture}>
+                [Picture Goes Here]
+            </div>
+
+            <div className={styles.bottomRow}>
+                <div className={styles.productName}>
+                    Product Name
+                </div>
+                <div className={styles.addToCart}>
+                    <TextButton buttonText="Add To List" buttonLink="ProductDetails"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+ProductCard2.PropTypes = {
+    buttonLink: PropTypes.string,
+}
+
+ProductCard2.defaultProps = {
+    buttonLink: "",
+}
+
+export default ProductCard2;
