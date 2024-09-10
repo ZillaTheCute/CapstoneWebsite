8059fc138a6631c4d66a98aadecddd5b120af022
Andrei Lee <andrei22.lee@gmail.com>
2024-09-10 22:30:48 +0800

    logo centered (vertically)

--- DCCPAI-Website/src/PageSections/Footer/Footer.jsx
+++ DCCPAI-Website/src/PageSections/Footer/Footer.jsx
@@ -6,26 +6,28 @@ function Footer() {
   return (
     <footer className={styles.footerSection}>
       <div className={styles.footerContainer}>
-        <div className={styles.footerLogoText}>
-        <Link to="/" className="Link">
-                        <img src={logoImage} className={styles.logoImage}></img>
-        </Link>
+        <div className={styles.leftMostLogo}>
+          <div className={styles.footerLogoText}>
+              <Link to="/" className="Link">
+                <img src={logoImage} className={styles.logoImage}></img>
+              </Link>
+            </div>
         </div>
         <div className={`${styles.footerFirstColumn} ${styles.footerColumn}`}>
           <ul className={styles.footerList}>
-          <Link to="/Shop" className="Link"><li>Shop</li></Link>
-          <Link to="/FindUs" className="Link"><li>Find Us</li></Link>
-          <Link to="/About" className="Link"><li>About Us</li></Link>
-          <Link to="/PrivacyPolicy" className="Link"><li>Privacy Policy</li></Link>
+            <Link to="/Shop" className="Link"><li>Shop</li></Link>
+            <Link to="/FindUs" className="Link"><li>Find Us</li></Link>
+            <Link to="/About" className="Link"><li>About Us</li></Link>
+            <Link to="/PrivacyPolicy" className="Link"><li>Privacy Policy</li></Link>
           </ul>
         </div>
         <div className={`${styles.footerSecondColumn} ${styles.footerColumn}`}>
           <ul className={styles.footerList}>
-          <Link to="/FAQ" className="Link"><li>Help & FAQ</li></Link>
-          {/* <Link to="/PageNotFound" className="Link"><li>Delivery (Tentative) </li></Link> */}
-          <Link to="/Returns" className="Link"><li>Returns</li></Link>
-          <Link to="/Payments" className="Link"><li>Payment Methods</li></Link>
-          {/* <Link to="/PageNotFound" className="Link"><li>Terms & Conditions (Tentative)</li></Link> */}
+            <Link to="/FAQ" className="Link"><li>Help & FAQ</li></Link>
+            {/* <Link to="/PageNotFound" className="Link"><li>Delivery (Tentative) </li></Link> */}
+            <Link to="/Returns" className="Link"><li>Returns</li></Link>
+            <Link to="/Payments" className="Link"><li>Payment Methods</li></Link>
+            {/* <Link to="/PageNotFound" className="Link"><li>Terms & Conditions (Tentative)</li></Link> */}
           </ul>
         </div>
         <div className={styles.footerContactText}>
