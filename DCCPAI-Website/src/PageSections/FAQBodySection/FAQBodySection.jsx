4d00a7a6bbe62013ec560f3809e1cc43a436455f
Andrei Lee <andrei22.lee@gmail.com>
2024-09-10 14:14:24 +0800

    FAQ common issues and height fixed, common issues converted to cards and height is auto instead of 100vh

--- DCCPAI-Website/src/PageSections/FAQBodySection/FAQBodySection.jsx
+++ DCCPAI-Website/src/PageSections/FAQBodySection/FAQBodySection.jsx
@@ -18,17 +18,25 @@ function FAQBodySection() {
         </div>
         
         <div className={styles.gridContainer}>
-        <FlavorTextLeftSmall mainText="How can I place an order?"
-                            subText="Placing an order is easy! Simply browse our catalog, add your desired items to the cart, and proceed to checkout. You can also place an order by contacting our customer service team at [phone number/email]."/>
-        
-        <FlavorTextLeftSmall mainText="How long does shipping take?"
-                            subText="Shipping times vary depending on your location and the shipping method selected. Typically, orders are processed within [time frame] and shipped within [time frame]. You can track your order through the link provided in your confirmation email."/>
-        
-        <FlavorTextLeftSmall mainText="Can I cancel or modify my order?"
-                            subText="If you need to cancel or modify your order, please contact us as soon as possible. We’ll do our best to accommodate your request if the order hasn’t been processed or shipped yet. For more information, visit our Order Cancellation Policy page."/>
-        
-        <FlavorTextLeftSmall mainText="Do you offer discounts or promotions?"
-                            subText="Yes, we regularly offer discounts and promotions. Be sure to sign up for our newsletter and follow us on social media to stay updated on the latest deals and special offers."/>
+            <div className={styles.FAQCard}>
+                <FlavorTextLeftSmall mainText="How can I place an order?"
+                                    subText="Placing an order is easy! Simply browse our catalog, add your desired items to the cart, and proceed to checkout. You can also place an order by contacting our customer service team at [phone number/email]."/>
+            </div>
+
+            <div className={styles.FAQCard}>
+                <FlavorTextLeftSmall mainText="How long does shipping take?"
+                                    subText="Shipping times vary depending on your location and the shipping method selected. Typically, orders are processed within [time frame] and shipped within [time frame]. You can track your order through the link provided in your confirmation email."/>
+            </div>
+
+            <div className={styles.FAQCard}>
+                <FlavorTextLeftSmall mainText="Can I cancel or modify my order?"
+                                    subText="If you need to cancel or modify your order, please contact us as soon as possible. We’ll do our best to accommodate your request if the order hasn’t been processed or shipped yet. For more information, visit our Order Cancellation Policy page."/>
+            </div>
+
+            <div className={styles.FAQCard}>
+                <FlavorTextLeftSmall mainText="Do you offer discounts or promotions?"
+                                    subText="Yes, we regularly offer discounts and promotions. Be sure to sign up for our newsletter and follow us on social media to stay updated on the latest deals and special offers."/>
+            </div>
         </div>
 
     </div>
