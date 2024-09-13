42f1b9e00a9772bf6b39c92037d76390879787a4
Andrei Lee <andrei22.lee@gmail.com>
2024-09-13 23:25:18 +0800

    About Page layout etc sort of finalized, created picture card for picture placeholders and whatnot.

--- DCCPAI-Website/src/PageSections/AboutBodySection/AboutBodySection.jsx
+++ DCCPAI-Website/src/PageSections/AboutBodySection/AboutBodySection.jsx
@@ -4,34 +4,48 @@ import FlavorTextWithButtonLeft from '../../FlavorTextWithButtonLeft/FlavorTextW
 import FlavorTextRight from '../../FlavorTextRight/FlavorTextRight.jsx'
 import FlavorTextWithButtonRight from '../../FlavorTextWithButtonRight/FlavorTextWithButtonRight.jsx'
 import FlavorTextMiddle from '../../FlavorTextMiddle/FlavorTextMiddle.jsx';
+import PictureCard from '../../PictureCard/PictureCard.jsx';
 
 function AboutBodySection() {
     return(
     <>
     <div className={styles.aboutBodyContainer}>
 
-        <div className={styles.leftText}>
-            <FlavorTextLeft mainText="The Mission" subText="To empower local cacao farmers by promoting sustainable farming practices, enhancing the quality and value of cacao products, 
-                                                            and fostering community-driven growth in the cacao industry. We are committed to building a resilient and prosperous future 
-                                                            for our members and their communities through education, innovation, and collaboration."/>
-        </div>
+        <div className={styles.missionVisionContainer}>
+            <div className={styles.leftTextTop}>
+                <FlavorTextLeft mainText="The Mission" subText="To empower local cacao farmers by promoting sustainable farming practices, enhancing the quality and value of cacao products, 
+                                                                and fostering community-driven growth in the cacao industry. We are committed to building a resilient and prosperous future 
+                                                                for our members and their communities through education, innovation, and collaboration."/>
+            </div>
 
-        <div className={styles.rightText}>
-            <FlavorTextRight mainText="The Vision" subText="To be a leading force in the global cacao industry, recognized for our dedication to quality, sustainability, and the empowerment 
-                                                            of local farmers. We envision a thriving cacao community where our products are celebrated worldwide, and our members enjoy economic 
-                                                            prosperity and a sustainable livelihood."/>
+            <div className={styles.flavorPicture}>
+                <PictureCard />
+            </div>
+
+            <div className={styles.leftTextBottom}>
+                <FlavorTextRight mainText="The Vision" subText="To be a leading force in the global cacao industry, recognized for our dedication to quality, sustainability, and the empowerment 
+                                                                of local farmers. We envision a thriving cacao community where our products are celebrated worldwide, and our members enjoy economic 
+                                                                prosperity and a sustainable livelihood."/>
+            </div>
         </div>
         
-        <div className={styles.leftText}>
-            <FlavorTextWithButtonLeft mainText="The Process (tentative)" buttonLabel="Video / placed at the side"/>
+        <div className={styles.theProcess}>
+            <PictureCard />
+            <div className={styles.leftText}>
+                <FlavorTextWithButtonLeft mainText="The Process" buttonLabel="Video"/>
+            </div>
         </div>
         
-        <div className={styles.rightText}>
-            <FlavorTextWithButtonRight mainText="The Source (tentative)" buttonLabel="Frequently Asked Questions" buttonLeadsTo="FAQ"/>
+        <div className={styles.theSource}>
+            <div className={styles.rightText}>
+                <FlavorTextWithButtonRight mainText="The Source" buttonLabel="Frequently Asked Questions" buttonLeadsTo="FAQ"/>
+            </div>
+            <PictureCard />
         </div>
+        
         
         <div className={styles.bottomText}>
-            <FlavorTextMiddle />
+            <FlavorTextMiddle mainText="We hope to see you." subText="We are located @ lorem ipsum address [insert physical store address here]"/>
         </div>
 
     </div>
--- DCCPAI-Website/src/PageSections/AboutTitleSection/AboutTitleSection.jsx
+++ DCCPAI-Website/src/PageSections/AboutTitleSection/AboutTitleSection.jsx
@@ -1,11 +1,15 @@
 import styles from '../AboutTitleSection/AboutTitleSection.module.css'
 import FlavorTextMiddle from '../../FlavorTextMiddle/FlavorTextMiddle';
+import sidePicture from '../../Assets/Pictures/shino-nakamura-QR07wCzQiJQ-unsplash.jpg'
+import PictureCard from '../../PictureCard/PictureCard';
+import FlavorTextLeft from '../../FlavorTextLeft/FlavorTextLeft';
 
 function AboutTitleSection() {
     return(
         <>
         <div className={styles.aboutTitleContainer}>
-            <FlavorTextMiddle mainText='About Us' 
+            <PictureCard imageSrc={sidePicture}/>
+            <FlavorTextLeft mainText='About Us' 
                                 subText="Since our inception in 2017, the Davao City Chocolate Processors Association Inc. (DCCPAI) has been dedicated to advancing cacao 
                                 production across the Davao Region. In line with our commitment to sustainable practices, we believe in producing high-quality 
                                 cacao and elevating the value of local cacao products."/> 
