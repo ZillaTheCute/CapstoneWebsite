d27db22d7743fd132993bb5c19c081715647de97
ZillaTheCute <104981138+ZillaTheCute@users.noreply.github.com>
2024-10-07 17:55:08 +0800

    Attempt 2 on getting it hosted

--- DCCPAI-Website/vite.config.js
+++ DCCPAI-Website/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CapstoneWebsite/', // Set your repository name here
+  base: '/dccpai-website/', // Set your repository name here
 })
